0abe86026a2a140307dbe5a5364430d92e32588f
Specify your username <Your email address>
2024-08-23 18:02:06 +0200

    push

--- src/views/AdminView.vue
+++ src/views/AdminView.vue
@@ -31,8 +31,8 @@
                         <div class="modal-dialog" role="document">
                             <div class="modal-content">
                             <div class="modal-header">
-                                <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
-                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <h5 class="modal-title" id="exampleModalLongTitle">Update User</h5>
+                                <button type="button"  data-dismiss="modal" aria-label="Close">
                                 <span aria-hidden="true">&times;</span>
                                 </button>
                             </div>
