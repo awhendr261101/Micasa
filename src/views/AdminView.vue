786e6994d5aba55e9e1f34a4f2361183c396be92
Specify your username <Your email address>
2024-08-26 13:15:27 +0200

    push

--- src/views/AdminView.vue
+++ src/views/AdminView.vue
@@ -304,12 +304,12 @@
                 <tr>
                   <td>
                     <!-- Button trigger modal -->
-                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#addProd">
                     ADD PRODUCT
                     </button>
 
                     <!-- Modal -->
-                    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal fade" id="addProd" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
                     <div class="modal-dialog" role="document">
                         <div class="modal-content">
                         <div class="modal-header">
